f9b938cbdf77a92e681d4925cd2d36b75d1c9d45
VB <vovanb@gmail.com>
2022-10-31 12:54:35 +0200

    Add project files.

---
+++ session1/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿
+
+namespace session1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+       
+        Microsoft.Maui.Handlers.WebViewHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+        {
+            if (view is MyCustomWebView)
+            {
+#if ANDROID
+                Android.Webkit.WebView webView = handler.PlatformView;
+                webView.Settings.JavaScriptEnabled = true;
+                webView.Settings.MixedContentMode = Android.Webkit.MixedContentHandling.AlwaysAllow;
+                webView.Settings.PluginsEnabled = true;
+                //webView.Settings.UserAgentString = 
+                
+                webView.Settings.AllowContentAccess = true;
+                webView.Settings.LightTouchEnabled = true;
+                webView.Settings.AllowFileAccess = true;
+                webView.Settings.SetSupportMultipleWindows(true);
+                webView.Settings.DatabaseEnabled = true;
+                webView.Settings.DatabasePath = "/data/data/session/databases/";
+                webView.Settings.DomStorageEnabled = true;
+                webView.Settings.SetAppCacheEnabled(true);
+                webView.Settings.SetAppCacheMaxSize(1024 * 1024 * 8);
+                webView.Settings.SetAppCachePath("/data/data/session/cache/");
+                webView.ClearCache(true);
+
+
+#endif
+            }
+        });
+
+    }
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
+
+}
+internal class MyCustomWebView : WebView
+{
+}
---
+++ session1/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using session1.Core;
+
+namespace session1;
+
+
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+
+        /*
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            count+=5;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            //Task.Run(async () =>
+            //{
+                 bool answer = await DisplayAlert("Attention", "Are you sure you want to cancel the transaction?" + CounterBtn.Text, "Yes", "No");
+
+            //});
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        */
+    }
+
+
